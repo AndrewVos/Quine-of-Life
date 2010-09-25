63b076a4d2215f40501e0782ee0a11ec8373a3f4
AndrewVos <andrew.vos@gmail.com>
2010-09-26 00:26:52 +0100

    Finished

--- test.rb
+++ test.rb
@@ -1,2 +1,3 @@
-exec "cp -n quine.rb output.rb;ruby output.rb | tee temp.rb;mv temp.rb output.rb"
+exec "cp quine.rb output.rb;while true; do clear; ruby output.rb | tee temp.rb; sleep 1; mv -f temp.rb output.rb; done"
+#exec "cp -n quine.rb output.rb;ruby output.rb | tee temp.rb;mv temp.rb output.rb"
 #exec "cp quine.rb output.rb;ruby output.rb | tee temp.rb;mv temp.rb output.rb;diff quine.rb output.rb;"
